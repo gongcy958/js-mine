ca90927415356e1df0d3cfffccc96f9d8f87e0a2
gongcy <gongchengyao9797@126.com>
2023-01-16 18:01:26 +0800

    test websocket

---
+++ html/pizza.py
@@ -0,0 +1,10 @@
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms','extra cheese'],
+    }
+
+print(pizza['toppings'])
+
+
+for topping in pizza['toppings']:
+    print(topping)
---
+++ html/toppings.py
@@ -0,0 +1,11 @@
+# requested_toppings = ['mushrooms','green peppers','extra cheese']
+# for requested_topping in requested_toppings:
+#     print(f"Adding {requested_topping}.")
+# print("\nFinished making your pizza!")
+
+requested_toppings = []
+
+if requested_toppings:
+    print("hello")
+else:
+    print("world")
