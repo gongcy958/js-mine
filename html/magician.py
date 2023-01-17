a1260f83fa74ba2ce56f52312aad88e32f5c28bb
gongcy <gongchengyao9797@126.com>
2023-01-17 09:25:36 +0800

    commit amend.

---
+++ html/magician.py
@@ -0,0 +1,5 @@
+magicians = ['alice','david','carolina']
+for magician in magicians:
+    print(f"{magician.title()},that was a great trick!")
+    print(f"I can't wait to see you next trick,{magician.title()}.\n")
+print("Thank you")
